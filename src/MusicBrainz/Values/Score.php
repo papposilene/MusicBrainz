83e44ac315975cb5fb431075d2c7c4e25a33d5c3
Christoph Hochholzer <christoph@hochholzer.info>
2017-03-02 23:28:46 +0100

    Added a value object for relevance scores.

---
+++ src/MusicBrainz/Values/Score.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MusicBrainz;
+
+/**
+ * A relevance score for search results
+ */
+class Score
+{
+    /**
+     * The relevance score
+     *
+     * @var string
+     */
+    private $score;
+
+    /**
+     * Constructs a relevance score.
+     *
+     * @param int $score
+     */
+    public function __construct(int $score)
+    {
+        if (0 <= $score && $score <= 100) {
+            $this->score = $score;
+
+            return;
+        }
+
+        if ($score < 0) {
+            $this->score = 0;
+        }
+
+        if ($score > 100) {
+            $this->score = 100;
+        }
+    }
+
+    /**
+     * Returns the relevance score as integer.
+     *
+     * @return int
+     */
+    public function toInteger(): int
+    {
+        return $this->score;
+    }
+}
