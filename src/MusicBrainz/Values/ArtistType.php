e7042f1a981db4dd5d144f2c4770e10954075c40
Christoph Hochholzer <christoph@hochholzer.info>
2017-03-02 21:00:11 +0100

    Added a value object for artist types.

---
+++ src/MusicBrainz/Values/ArtistType.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace MusicBrainz;
+
+/**
+ * An artist type
+ *
+ * @see https://musicbrainz.org/doc/Artist
+ */
+class ArtistType
+{
+    /**
+     * This indicates an individual person.
+     *
+     * @var string
+     */
+    public const PERSON = 'Person';
+
+    /**
+     * This indicates a group of people that may or may not have a distinctive name.
+     *
+     * @var string
+     */
+    public const GROUP = 'Group';
+
+    /**
+     * This indicates an orchestra (a large instrumental ensemble).
+     *
+     * @var string
+     */
+    public const ORCHESTRA = 'Orchestra';
+
+    /**
+     * This indicates a choir/chorus (a large vocal ensemble).
+     *
+     * @var string
+     */
+    public const CHOIR = 'Choir';
+
+    /**
+     * This indicates an individual fictional character.
+     *
+     * @var string
+     */
+    public const CHARACTER = 'Character';
+
+    /**
+     * This indicates an undefined type of character.
+     *
+     * @var string
+     */
+    public const OTHER = 'Other';
+
+    /**
+     * A list of valid artist type codes
+     *
+     * @var array
+     */
+    private const TYPES = [
+        self::PERSON,
+        self::GROUP,
+        self::ORCHESTRA,
+        self::CHOIR,
+        self::CHARACTER,
+        self::OTHER
+        ];
+
+    /**
+     * The artist type code
+     *
+     * @var string
+     */
+    private $type;
+
+    /**
+     * Constructs an artist type.
+     *
+     * @param string $type
+     */
+    public function __construct(string $type)
+    {
+        if (in_array($type, self::TYPES)) {
+            $this->type = $type;
+        }
+
+        $this->type = self::OTHER;
+    }
+
+    /**
+     * Returns the artist type code.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->type;
+    }
+}
