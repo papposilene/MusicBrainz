f74b418da9e6f546d7253c392c6e04425c181af8
Christoph Hochholzer <christoph@hochholzer.info>
2017-03-06 23:02:34 +0100

    Implemented tag list object.

---
+++ src/MusicBrainz/Value/TagList.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * A list of tags
+ *
+ * @see https://musicbrainz.org/doc/Aliases
+ */
+class TagList extends ValueList
+{
+    /**
+     * Constructs a list of tags.
+     *
+     * @param array $tags An array tag arrays
+     */
+    public function __construct(array $tags = array())
+    {
+        parent::__construct(
+            array_map(
+                function($tag) {
+                    return new Tag($tag);
+                },
+                $tags
+            )
+        );
+    }
+
+    /**
+     * Returns the class name of the list elements.
+     *
+     * @return string
+     */
+    public static function getType(): string
+    {
+        return Tag::class;
+    }
+}
