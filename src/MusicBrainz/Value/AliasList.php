aee87882175c59ec0243f848c07c27ec15df98a3
Christoph Hochholzer <christoph@hochholzer.info>
2017-03-06 23:03:09 +0100

    Implemented alias list object.

---
+++ src/MusicBrainz/Value/AliasList.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * A list of aliases
+ *
+ * @see https://musicbrainz.org/doc/Aliases
+ */
+class AliasList extends ValueList
+{
+    /**
+     * Constructs a list of aliases.
+     *
+     * @param array $aliases An array alias arrays
+     */
+    public function __construct(array $aliases = array())
+    {
+        parent::__construct(
+            array_map(
+                function($alias) {
+                    return new Alias($alias);
+                },
+                $aliases
+            )
+        );
+    }
+
+    /**
+     * Returns the class name of the list elements.
+     *
+     * @return string
+     */
+    public static function getType(): string
+    {
+        return Alias::class;
+    }
+}
