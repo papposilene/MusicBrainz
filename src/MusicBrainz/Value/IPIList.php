eba20982b6f4334d46ec6a5c6acc6b42fa791d2b
Christoph Hochholzer <christoph@hochholzer.info>
2017-03-06 23:02:50 +0100

    Implemented IPI list object.

---
+++ src/MusicBrainz/Value/IPIList.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * A list of IPI codes
+ *
+ * @see https://musicbrainz.org/doc/IPI
+ */
+class IPIList extends ValueList
+{
+    /**
+     * Constructs a list of IPI codes.
+     *
+     * @param array $ipiCodes An array of IPI code strings
+     */
+    public function __construct(array $ipiCodes = array())
+    {
+        parent::__construct(
+            array_map(
+                function($ipiCode) {
+                    return new IPI($ipiCode);
+                },
+                $ipiCodes
+            )
+        );
+    }
+
+    /**
+     * Returns the class name of the list elements.
+     *
+     * @return string
+     */
+    public static function getType(): string
+    {
+        return IPI::class;
+    }
+}
