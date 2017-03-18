8f73975ad5556d4b1764eb32f65cf1f76237517c
Christoph Hochholzer <christoph@hochholzer.info>
2017-03-19 00:00:45 +0100

    Added recording fields.

---
+++ src/MusicBrainz/Api/Lookup/Field/ArtistCreditsTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MusicBrainz\Api\Lookup\Field;
+
+trait ArtistCreditsTrait
+{
+    /**
+     * True, if artist credits should be included, otherwise false
+     *
+     * @var bool
+     */
+    private $artistCredits = false;
+
+    /**
+     * Returns true, if artist credits should be included, otherwise false.
+     *
+     * @return bool
+     */
+    public function isArtistCredits(): bool
+    {
+        return $this->artistCredits;
+    }
+
+    /**
+     * Sets whether artist credits should be included.
+     *
+     * @param bool $artistCredits True, if artist credits should be included, otherwise false
+     *
+     * @return self
+     */
+    public function setArtistCredits(bool $artistCredits = true): self
+    {
+        $this->artistCredits = $artistCredits;
+
+        return $this;
+    }
+}
