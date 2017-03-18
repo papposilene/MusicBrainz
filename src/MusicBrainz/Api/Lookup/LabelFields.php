d2f6bcba7e85e5c67ea1368512adee69ac34e2c5
Christoph Hochholzer <christoph@hochholzer.info>
2017-03-18 23:53:51 +0100

    Implemented label fields.

---
+++ src/MusicBrainz/Api/Lookup/LabelFields.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MusicBrainz\Api\Lookup;
+
+class LabelFields
+{
+    use Field\ReleasesTrait;
+    use Field\DiscIdsTrait;
+    use Field\MediaTrait;
+    use Field\AliasesTrait;
+    use Field\TagsTrait;
+    use Field\UserTagsTrait;
+    use Field\RatingsTrait;
+    use Field\UserRatingsTrait;
+    use Field\ArtistRelationsTrait;
+    use Field\LabelRelationsTrait;
+    use Field\RecordingRelationsTrait;
+    use Field\ReleaseRelationsTrait;
+    use Field\ReleaseGroupsTrait;
+    use Field\URLRelationsTrait;
+    use Field\WorkRelationsTrait;
+    use Field\AnnotationTrait;
+}
