32ac6faca9e62315047e676d5e38b1f319826f68
Christoph Hochholzer <christoph@hochholzer.info>
2017-03-21 20:59:52 +0100

    Added a value object for the artist credit.

---
+++ src/MusicBrainz/Value/ArtistCredit.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * An artist credit
+ */
+class ArtistCredit
+{
+    use Property\NameTrait;
+    use Property\ArtistTrait;
+    use Property\JoinPhraseTrait;
+
+    /**
+     * Constructs an artist credit.
+     *
+     * @param array $artistCredit An array of information about the artist credit
+     */
+    public function __construct(array $artistCredit)
+    {
+        $this->name       = new Name(isset($artistCredit['name']) ? (string) $artistCredit['name'] : '');
+        $this->artist     = new ArtistType(isset($artistCredit['artist']) ? (string) $artistCredit['artist'] : '');
+        $this->joinPhrase = new JoinPhrase(isset($artistCredit['joinphrase']) ? (string) $artistCredit['joinphrase'] : '');
+    }
+
+    /**
+     * Returns the artist credit as string.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        $joinPhrase = (empty($this->joinPhrase)) ? '' : $this->joinPhrase . ' ';
+        $joinPhrase .= $this->name;
+
+        return $joinPhrase;
+    }
+}
