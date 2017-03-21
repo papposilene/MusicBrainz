0f4b32abcad13a7b8db1af1f0cb874c294a00057
Christoph Hochholzer <christoph@hochholzer.info>
2017-03-21 21:07:26 +0100

    Added artist credit list.

---
+++ src/MusicBrainz/Value/ArtistCreditList.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * A list of artist credits
+ */
+class ArtistCreditList extends ValueList
+{
+    /**
+     * Constructs a list of artist credits.
+     *
+     * @param array $artistCredits An array of information about the artist credits
+     */
+    public function __construct(array $artistCredits = [])
+    {
+        parent::__construct(
+            array_map(
+                function($artistCredit) {
+                    return new Artist($artistCredit);
+                },
+                $artistCredits
+            )
+        );
+    }
+
+    /**
+     * Returns the class name of the list elements.
+     *
+     * @return string
+     */
+    public static function getType(): string
+    {
+        return ArtistCredit::class;
+    }
+}
