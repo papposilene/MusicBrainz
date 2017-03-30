c0e36e015058a57784537ea89cf28ecc4ac8cddf
Christoph Hochholzer <christoph@hochholzer.info>
2017-03-30 16:04:46 +0200

    Allow several date formats.

--- src/MusicBrainz/Value/Date.php
+++ src/MusicBrainz/Value/Date.php
@@ -4,18 +4,18 @@ namespace MusicBrainz\Value;
 
 /**
  * A date
- *
- * @see https://musicbrainz.org/doc/IPI
  */
 class Date
 {
     /**
-     * The format for constructing a date
-     * YYYY-MM-DD (leading zeros for months and days)
+     * The original given date format
+     * This value is necessary to interpret the date time object correctly. As the date time object always points to
+     * an exact point of time, it will point to 2017/01/01 at 00:00 a. m. if constructed by the string "2017" that
+     * intends to mean "sometime in 2017".
      *
-     * @var string
+     * @var null|string
      */
-    private const FORMAT = 'Y-m-d';
+    private $originalFormat;
 
     /**
      * Date and time
@@ -31,11 +31,74 @@ class Date
      */
     public function __construct(string $date = '')
     {
-        $dateTime = \DateTime::createFromFormat(self::FORMAT, $date);
+        if (empty($date)) {
 
-        $this->dateTime = ($dateTime && $dateTime->format(self::FORMAT) === $date)
+            return;
+        }
+
+        $this->originalFormat = $this->getFormat($date);
+
+        if (null === $this->originalFormat) {
+
+            return;
+        }
+
+        $dateTime = \DateTime::createFromFormat('!' . $this->originalFormat, $date);
+
+        $this->dateTime = ($dateTime && $dateTime->format($this->originalFormat) === $date)
             ? $dateTime
             : null;
+    }
+
+    /**
+     * Returns the date time format by a given date string.
+     * Supported formats are:
+     * - Y-m-d: 2017-03-01
+     * - Y-m: 2017-03
+     * - Y: 2017
+     *
+     * @param string $date A date string
+     *
+     * @return null|string
+     */
+    private function getFormat(string $date): ?string
+    {
+        if (preg_match('/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/',$date)) {
+
+            return 'Y-m-d';
+            }
+
+        if (preg_match('/^[0-9]{4}-(0[1-9]|1[0-2])$/',$date)) {
+
+            return 'Y-m';
+        }
+
+        if (preg_match('/^[0-9]{4}$/',$date)) {
+
+            return 'Y';
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns the date time object.
+     *
+     * @return null|\DateTime
+     */
+    public function getDateTime(): ?\DateTime
+    {
+        return $this->dateTime;
+    }
+
+    /**
+     * Returns the original format.
+     *
+     * @return null|string
+     */
+    public function getOriginalFormat(): ?string
+    {
+        return $this->originalFormat;
     }
 
     /**
@@ -46,7 +109,7 @@ class Date
     public function __toString(): string
     {
         return ($this->dateTime)
-            ? $this->dateTime->format(self::FORMAT)
+            ? $this->dateTime->format($this->originalFormat)
             : '';
     }
 }
