a69cc2063aa74a9dfff050fd30eee767ea27bef9
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-01 11:55:31 +0200

    Add documentation to the recording value.

--- src/MusicBrainz/Value/Recording.php
+++ src/MusicBrainz/Value/Recording.php
@@ -4,6 +4,14 @@ namespace MusicBrainz\Value;
 
 /**
  * A recording
+ * A recording is an entity in MusicBrainz which can be linked to tracks on releases. Each track must always be
+ * associated with a single recording, but a recording can be linked to any number of tracks.
+ * A recording represents distinct audio that has been used to produce at least one released track through copying or
+ * mastering. A recording itself is never produced solely through copying or mastering.
+ * Generally, the audio represented by a recording corresponds to the audio at a stage in the production process before
+ * any final mastering but after any editing or mixing.
+ *
+ * @link https://musicbrainz.org/doc/Recording
  */
 class Recording
 {
