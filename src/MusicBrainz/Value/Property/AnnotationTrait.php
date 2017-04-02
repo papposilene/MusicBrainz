c983c20db8719ac8f1deec215c9bc26559ba48a5
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-02 11:58:10 +0200

    Added property trait for the annotation.

---
+++ src/MusicBrainz/Value/Property/AnnotationTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MusicBrainz\Value\Property;
+
+use MusicBrainz\Value\Annotation;
+
+/**
+ * Provides a getter for a annotation.
+ */
+trait AnnotationTrait
+{
+    /**
+     * The annotation
+     *
+     * @var Annotation
+     */
+    public $annotation;
+
+    /**
+     * Returns the annotation.
+     *
+     * @return Annotation
+     */
+    public function getAnnotation(): Annotation
+    {
+        return $this->annotation;
+    }
+}
