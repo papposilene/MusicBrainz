accdb11fc49ff9e26d59942884abde803b932310
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-10 23:05:03 +0200

    Added constants for entity types.

--- src/MusicBrainz/Value/EntityType.php
+++ src/MusicBrainz/Value/EntityType.php
@@ -8,6 +8,132 @@ namespace MusicBrainz\Value;
 class EntityType
 {
     /**
+     * An area
+     *
+     * @var string
+     */
+    public const AREA = 'area';
+
+    /**
+     * An artist
+     *
+     * @var string
+     */
+    public const ARTIST = 'artist';
+
+    /**
+     * A collection
+     *
+     * @var string
+     */
+    public const COLLECTION = 'collection';
+
+    /**
+     * An editor
+     *
+     * @var string
+     */
+    public const EDITOR = 'editor';
+
+    /**
+     * An event
+     *
+     * @var string
+     */
+    public const EVENT = 'event';
+
+    /**
+     * A label
+     *
+     * @var string
+     */
+    public const LABEL = 'label';
+
+    /**
+     * A place
+     *
+     * @var string
+     */
+    public const PLACE = 'place';
+
+    /**
+     * A recording
+     *
+     * @var string
+     */
+    public const RECORDING = 'recording';
+
+    /**
+     * A recource
+     *
+     * @var string
+     */
+    public const RECOURCE = 'recource';
+
+    /**
+     * A release
+     *
+     * @var string
+     */
+    public const RELEASE = 'release';
+
+    /**
+     * A release group
+     *
+     * @var string
+     */
+    public const RELEASE_GROUP = 'release-group';
+
+    /**
+     * A track
+     *
+     * @var string
+     */
+    public const TRACK = 'track';
+
+    /**
+     * An URL
+     *
+     * @var string
+     */
+    public const URL = 'url';
+
+    /**
+     * A work
+     *
+     * @var string
+     */
+    public const WORK = 'work';
+
+    /**
+     * An undefined entity type
+     *
+     * @var string
+     */
+    public const UNDEFINED = '';
+
+    /**
+     * A list of valid entity types
+     *
+     * @var array
+     */
+    private const ENTITY_TYPES = [
+        self::AREA,
+        self::ARTIST,
+        self::COLLECTION,
+        self::EDITOR,
+        self::EVENT,
+        self::LABEL,
+        self::PLACE,
+        self::RECORDING,
+        self::RECOURCE,
+        self::RELEASE,
+        self::RELEASE_GROUP,
+        self::TRACK,
+        self::WORK
+    ];
+
+    /**
      * The entity type code
      *
      * @var string
@@ -19,9 +145,11 @@ class EntityType
      *
      * @param string $entityTypeCode The entity type code
      */
-    public function __construct(string $entityTypeCode = '')
+    public function __construct(string $entityTypeCode = self::UNDEFINED)
     {
-        $this->entityTypeCode = $entityTypeCode;
+        $this->entityTypeCode = in_array($entityTypeCode, self::ENTITY_TYPES)
+            ? $entityTypeCode
+            : self::UNDEFINED;
     }
 
     /**
