5dec4620f768aca8038fd267c0c5d421b7d35ea6
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-11 22:24:14 +0200

    Added release filter.

---
+++ src/MusicBrainz/Filter/Browse/ReleaseFilter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MusicBrainz\Filter\Search;
+
+/**
+ * A filter for releases
+ */
+class ReleaseFilter
+{
+    use \MusicBrainz\Filter\Property\ReleaseStatusTrait;
+    use \MusicBrainz\Filter\Property\ReleaseTypeTrait;
+}
