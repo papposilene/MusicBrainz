7820df037753ed649123aa16fcb4e6c99eb21557
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-11 22:35:22 +0200

    Added event list object.

---
+++ src/MusicBrainz/Value/EventList.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * A list of events
+ */
+class EventList extends ValueList
+{
+    /**
+     * Constructs a list of events.
+     *
+     * @param array $events An array containing information about the events
+     */
+    public function __construct(array $events = [])
+    {
+        parent::__construct(
+            array_map(
+                function($event) {
+                    return new Event($event);
+                },
+                $events
+            )
+        );
+    }
+
+    /**
+     * Returns the class name of the list elements.
+     *
+     * @return string
+     */
+    public static function getType(): string
+    {
+        return Event::class;
+    }
+}
