c5cf4e9b501b615d103bbbfa83ded3d6e3bba7d9
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-11 22:56:09 +0200

    Added ISNI list to the label.

--- src/MusicBrainz/Value/Label.php
+++ src/MusicBrainz/Value/Label.php
@@ -7,19 +7,19 @@ namespace MusicBrainz\Value;
  */
 class Label
 {
-    use Property\MBIDTrait;
-    use Property\NameTrait;
     use Property\AliasesTrait;
-    use Property\IpisTrait;
+    use Property\AreaTrait;
     use Property\CountryTrait;
+    use Property\DisambiguationTrait;
+    use Property\IpisTrait;
+    use Property\IsnisTrait;
     use Property\LabelCodeTrait;
     use Property\LabelTypeTrait;
-    use Property\AreaTrait;
     use Property\LifeSpanTrait;
-    use Property\SortNameTrait;
-    use Property\IsnisTrait;
-    use Property\DisambiguationTrait;
+    use Property\MBIDTrait;
+    use Property\NameTrait;
     use Property\ScoreTrait;
+    use Property\SortNameTrait;
 
     /**
      * Constructs a label.
@@ -28,17 +28,18 @@ class Label
      */
     public function __construct(array $label = [])
     {
-        $this->MBID           = new MBID(isset($label['id']) ? (string) $label['id'] : '');
-        $this->name           = new Name(isset($label['name']) ? (string) $label['name'] : '');
         $this->aliases        = new AliasList(isset($label['aliases']) ? $label['aliases'] : []);
-        $this->ipis           = new IPIList(isset($label['ipis']) ? $label['ipis'] : []);
+        $this->area           = new Area(isset($label['area']) ? $label['area'] : []);
         $this->country        = new Country(isset($label['country']) ? (string) $label['country'] : '');
+        $this->disambiguation = new Disambiguation(isset($label['disambiguation']) ? (string)$label['disambiguation'] : '');
+        $this->ipis           = new IPIList(isset($label['ipis']) ? $label['ipis'] : []);
+        $this->isnis          = new ISNIList(isset($label['isnis']) ? $label['isnis'] : []);
         $this->labelCode      = new LabelCode(isset($label['label-code']) ? (string) $label['label-code'] : '');
         $this->labelType      = new LabelType(isset($label['type']) ? (string) $label['type'] : '');
-        $this->area           = new Area(isset($label['area']) ? $label['area'] : []);
         $this->lifeSpan       = new LifeSpan(isset($label['life-span']) ? $label['life-span'] : []);
-        $this->sortName       = new SortName(isset($label['sort-name']) ? (string)$label['sort-name'] : '');
-        $this->disambiguation = new Disambiguation(isset($label['disambiguation']) ? (string)$label['disambiguation'] : '');
+        $this->MBID           = new MBID(isset($label['id']) ? (string) $label['id'] : '');
+        $this->name           = new Name(isset($label['name']) ? (string) $label['name'] : '');
         $this->score          = isset($label['score']) ? (int)$label['score'] : 0;
+        $this->sortName       = new SortName(isset($label['sort-name']) ? (string)$label['sort-name'] : '');
     }
 }
