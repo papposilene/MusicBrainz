a7734a8fd09ddf2b2eece3f783951fee5db4d749
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-13 20:02:59 +0200

    Added the text representation to the release.

--- src/MusicBrainz/Value/Release.php
+++ src/MusicBrainz/Value/Release.php
@@ -18,6 +18,7 @@ class Release
     use Property\MediaTrait;
     use Property\PackagingTrait;
     use Property\TitleTrait;
+    use Property\TextRepresentationTrait;
 
     /**
      * Constructs a recording.
@@ -26,16 +27,17 @@ class Release
      */
     public function __construct(array $release = [])
     {
-        $this->aliases        = new AliasList(isset($release['aliases']) ? $release['aliases'] : []);
-        $this->annotation     = new Annotation(isset($release['annotation']) ? $release['annotation'] : []);
-        $this->artistCredits  = new ArtistCreditList(isset($release['artistCredits']) ? (string) $release['artistCredits'] : []);
-        $this->asin           = new ASIN(isset($release['asin']) ? (string) $release['asin'] : '');
-        $this->barcode        = new Barcode(isset($release['barcode']) ? (string) $release['barcode'] : '');
-        $this->date           = new Date(isset($release['date']) ? (string) $release['date'] : '');
-        $this->disambiguation = new Disambiguation(isset($release['disambiguation']) ? (string) $release['disambiguation'] : '');
-        $this->MBID           = new MBID(isset($release['id']) ? (string) $release['id'] : '');
-        $this->media          = new MediaList(isset($release['media']) ? $release['media'] : []);
-        $this->packaging      = new Packaging(isset($release['packaging']) ? $release['packaging'] : '');
-        $this->title          = new Title(isset($release['title']) ? $release['title'] : '');
+        $this->aliases            = new AliasList(isset($release['aliases']) ? $release['aliases'] : []);
+        $this->annotation         = new Annotation(isset($release['annotation']) ? $release['annotation'] : []);
+        $this->artistCredits      = new ArtistCreditList(isset($release['artistCredits']) ? (string) $release['artistCredits'] : []);
+        $this->asin               = new ASIN(isset($release['asin']) ? (string) $release['asin'] : '');
+        $this->barcode            = new Barcode(isset($release['barcode']) ? (string) $release['barcode'] : '');
+        $this->date               = new Date(isset($release['date']) ? (string) $release['date'] : '');
+        $this->disambiguation     = new Disambiguation(isset($release['disambiguation']) ? (string) $release['disambiguation'] : '');
+        $this->MBID               = new MBID(isset($release['id']) ? (string) $release['id'] : '');
+        $this->media              = new MediaList(isset($release['media']) ? $release['media'] : []);
+        $this->packaging          = new Packaging(isset($release['packaging']) ? $release['packaging'] : '');
+        $this->title              = new Title(isset($release['title']) ? $release['title'] : '');
+        $this->textRepresentation = new TextRepresentation(isset($release['text-representation']) ? $release['text-representation'] : []);
     }
 }
