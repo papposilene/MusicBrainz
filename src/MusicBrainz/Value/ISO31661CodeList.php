eb28eaed3ed5ea47b4bfe42c81989f73518177bd
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-14 19:10:34 +0200

    Added list object for ISO 3166-1 codes.

---
+++ src/MusicBrainz/Value/ISO31661CodeList.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * A list of ISO 3166-1 codes
+ */
+class ISO31661CodeList extends ValueList
+{
+    /**
+     * Constructs a list of ISO 3166-1 codes.
+     *
+     * @param array $codes Information about the ISO 3166-1 codes
+     */
+    public function __construct(array $codes = [])
+    {
+        parent::__construct(
+            array_map(
+                function($code) {
+                    return new ISO31661Code($code);
+                },
+                $codes
+            )
+        );
+    }
+
+    /**
+     * Returns the class name of the list elements.
+     *
+     * @return string
+     */
+    public static function getType(): string
+    {
+        return ISO31661Code::class;
+    }
+}
