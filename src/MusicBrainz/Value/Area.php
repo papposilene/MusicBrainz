24f4666c071ea211a776062f193f40f359326b15
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-14 19:11:39 +0200

    Added ISO 3166-1 codes to the area.

--- src/MusicBrainz/Value/Area.php
+++ src/MusicBrainz/Value/Area.php
@@ -9,6 +9,7 @@ class Area
 {
     use Property\AreaTypeTrait;
     use Property\DisambiguationTrait;
+    use Property\ISO31661CodesTrait;
     use Property\ISO31662CodesTrait;
     use Property\LifeSpanTrait;
     use Property\MBIDTrait;
@@ -24,6 +25,7 @@ class Area
     {
         $this->areaType       = new AreaType(isset($area['type']) ? $area['type'] : '');
         $this->disambiguation = new Disambiguation(isset($area['disambiguation']) ? $area['disambiguation'] : '');
+        $this->ISO31661Codes  = new ISO31662CodeList(isset($area['iso-3166-1-codes']) ? $area['iso-3166-1-codes'] : []);
         $this->ISO31662Codes  = new ISO31662CodeList(isset($area['iso-3166-2-codes']) ? $area['iso-3166-2-codes'] : []);
         $this->lifeSpan       = new LifeSpan(isset($area['life-span']) ? $area['life-span'] : []);
         $this->MBID           = new MBID(isset($area['id']) ? (string) $area['id'] : '');
