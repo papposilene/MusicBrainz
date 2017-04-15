8a71b8b821a103abfeb44a0977faafd86ce66d62
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-15 17:52:42 +0200

    Added the collection list.

---
+++ src/MusicBrainz/Value/CollectionList.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * A list of collections
+ */
+class CollectionList extends ValueList
+{
+    /**
+     * Constructs a list of collections.
+     *
+     * @param array $collections Information about the collections
+     */
+    public function __construct(array $collections = [])
+    {
+        parent::__construct(
+            array_map(
+                function($collection) {
+                    return new Collection($collection);
+                },
+                $collections
+            )
+        );
+    }
+
+    /**
+     * Returns the class name of the list elements.
+     *
+     * @return string
+     */
+    public static function getType(): string
+    {
+        return Collection::class;
+    }
+}
