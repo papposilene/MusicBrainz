5beefccfa75d6a7087cd03d95f2022fcdd3fb87d
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-16 11:37:41 +0200

    Added a value object for the instrument.

---
+++ src/MusicBrainz/Value/Instrument.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * An instrument
+ *
+ * @link https://musicbrainz.org/instruments
+ */
+class Instrument
+{
+    use Property\Description;
+    use Property\DisambiguationTrait;
+    use Property\InstrumentNameTrait;
+    use Property\InstrumentTypeTrait;
+    use Property\MBIDTrait;
+
+    /**
+     * Constructs an instrument.
+     *
+     * @param array $instrument Information about an instrument
+     */
+    public function __construct(array $instrument = [])
+    {
+        $this->description    = new Description(isset($instrument['description']) ? $instrument['description'] : '');
+        $this->disambiguation = new Disambiguation(isset($instrument['disambiguation']) ? (string) $instrument['disambiguation'] : '');
+        $this->instrumentName = new InstrumentName(isset($instrument['name']) ? $instrument['name'] : []);
+        $this->instrumentType = new InstrumentType(isset($instrument['type']) ? $instrument['type'] : []);
+        $this->MBID           = new MBID(isset($instrument['id']) ? (string) $instrument['id'] : '');
+    }
+
+    /**
+     * Returns the instrument name.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return (string) $this->getInstrumentName();
+    }
+}
