1a4ddc404ebe0cbfa6b1c38b663e793ceee9d897
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-16 12:22:38 +0200

    Added trait for including collections.

---
+++ src/MusicBrainz/Supplement/Field/CollectionsTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MusicBrainz\Supplement\Field;
+
+trait CollectionsTrait
+{
+    /**
+     * True, if collections should be included, otherwise false
+     *
+     * @var bool
+     */
+    private $collections = false;
+
+    /**
+     * Returns true, if collections should be included, otherwise false.
+     *
+     * @return bool
+     */
+    public function isCollections(): bool
+    {
+        return $this->collections;
+    }
+
+    /**
+     * Sets whether collections should be included.
+     *
+     * @param bool $collections True, if collections should be included, otherwise false
+     *
+     * @return self
+     */
+    public function setCollections(bool $collections = true): self
+    {
+        $this->collections = $collections;
+
+        return $this;
+    }
+}
