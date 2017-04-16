4389a52c0cac02f366c47ff2a2e746675095488e
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-16 14:55:34 +0200

    Added a list object for release types.

---
+++ src/MusicBrainz/Value/ReleaseTypeList.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * A list of release types
+ */
+class ReleaseTypeList extends ValueList
+{
+    /**
+     * Constructs a list of release types.
+     *
+     * @param array $releaseTypes Information about the releaseTypes
+     */
+    public function __construct(array $releaseTypes = [])
+    {
+        parent::__construct(
+            array_map(
+                function($releaseType) {
+                    return new ReleaseType($releaseType);
+                },
+                $releaseTypes
+            )
+        );
+    }
+
+    /**
+     * Returns the class name of the list elements.
+     *
+     * @return string
+     */
+    public static function getType(): string
+    {
+        return ReleaseType::class;
+    }
+}
