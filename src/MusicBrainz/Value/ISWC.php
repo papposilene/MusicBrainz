d40395e9702ed1f126a0d8f0b1335264c94986c6
Christoph Hochholzer <christoph@hochholzer.info>
2017-04-16 18:36:45 +0200

    Added a value object for an ISWC code.

---
+++ src/MusicBrainz/Value/ISWC.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * An International Standard Musical Work Code (ISWC)
+ * The International Standard Musical Work Code (ISWC) is an ISO standard similar to ISBNs for identifying musical
+ * works/compositions.
+ *
+ * @link https://musicbrainz.org/doc/ISWC
+ */
+class ISWC
+{
+    /**
+     * The code
+     *
+     * @var string
+     */
+    private $code;
+
+    /**
+     * Constructs an ISWC.
+     *
+     * @param string $code An ISWC
+     */
+    public function __construct(string $code = '')
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * Returns the ISWC as string.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->code;
+    }
+}
