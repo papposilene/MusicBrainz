2f3fbc97c78726085db94a1321ab113624d44f96
Christoph Hochholzer <christoph@hochholzer.info>
2017-06-07 17:54:00 +0200

    Added a list object for places.

---
+++ src/MusicBrainz/Value/PlaceList.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * A list of places
+ */
+class PlaceList extends ValueList
+{
+    /**
+     * Constructs a list of places.
+     *
+     * @param array $places An array containing information about the places
+     */
+    public function __construct(array $places = [])
+    {
+        parent::__construct(
+            array_map(
+                function($place) {
+                    return new Place($place);
+                },
+                $places
+            )
+        );
+    }
+
+    /**
+     * Returns the class name of the list elements.
+     *
+     * @return string
+     */
+    public static function getType(): string
+    {
+        return Place::class;
+    }
+}
