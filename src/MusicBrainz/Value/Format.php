1d93470d6b27f43800d4164ea7f045199ed8b05a
Christoph Hochholzer <christoph@hochholzer.info>
2017-06-09 23:33:29 +0200

    Added a value object for the release format.

---
+++ src/MusicBrainz/Value/Format.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * A releases format
+ *
+ * @see https://musicbrainz.org/doc/Release/Format
+ */
+class Format
+{
+    /**
+     * The code of the format
+     *
+     * @var string
+     */
+    private $code;
+
+    /**
+     * Constructs a format.
+     *
+     * @param string $format A format
+     */
+    public function __construct(string $format = '')
+    {
+        $this->code = $format;
+    }
+
+    /**
+     * Returns the code of the format.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->code;
+    }
+}
