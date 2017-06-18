05b834ce659bdced51bd03781728aaf59929db29
Christoph Hochholzer <christoph@hochholzer.info>
2017-06-18 23:32:25 +0200

    Added the series relation object.

---
+++ src/MusicBrainz/Relation/Entity/SeriesRelation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MusicBrainz\Relation\Entity;
+
+use MusicBrainz\Relation\AbstractRelation;
+
+/**
+ * A relation between a series and another entity.
+ */
+class SeriesRelation extends AbstractRelation
+{
+    use \MusicBrainz\Relation\RelatedEntity\CollectionTrait;
+}
