e9b9d6eb19790ef791d65619497b45bed92feeb7
Christoph Hochholzer <christoph@hochholzer.info>
2017-06-18 19:40:07 +0200

    Implemented logger.

--- src/MusicBrainz/MusicBrainz.php
+++ src/MusicBrainz/MusicBrainz.php
@@ -2,7 +2,10 @@
 
 namespace MusicBrainz;
 
+use GuzzleHttp\Tests\Logger;
 use MusicBrainz\HttpAdapter\AbstractHttpAdapter;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 /**
  * Connect to the MusicBrainz web service
@@ -26,14 +29,22 @@ class MusicBrainz
     private $config;
 
     /**
+     * A logger
+     *
+     * @var LoggerInterface
+     */
+    private static $logger;
+
+    /**
      * Constructs the MusicBrainz API client.
      *
      * @param HttpAdapter\AbstractHttpAdapter $adapter The Http adapter used to make requests
      */
-    public function __construct(AbstractHttpAdapter $adapter)
+    public function __construct(AbstractHttpAdapter $adapter, LoggerInterface $logger = null)
     {
         $this->adapter = $adapter;
         $this->config  = new Config;
+        self::setLogger($logger);
     }
 
     /**
@@ -54,5 +65,28 @@ class MusicBrainz
     public function config(): Config
     {
         return $this->config;
+    }
+
+    /**
+     * Returns the logger.
+     *
+     * @return LoggerInterface
+     */
+    public static function log(): LoggerInterface
+    {
+        return (is_null(self::$logger)) ? self::setLogger() : self::$logger;
+    }
+
+    /**
+     * Sets a given logger statically and returns it. Creates a new null object logger, sets it statically and returns
+     * it, if no logger was given.
+     *
+     * @param null|LoggerInterface $logger A logger
+     *
+     * @return LoggerInterface
+     */
+    private static function setLogger(LoggerInterface $logger = null): LoggerInterface
+    {
+        return self::$logger = (is_null($logger)) ? new NullLogger : $logger;
     }
 }
