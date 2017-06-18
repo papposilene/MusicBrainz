e8e98aa0bfeca0c28e0f7462426f507cd8870624
Christoph Hochholzer <christoph@hochholzer.info>
2017-06-18 23:34:00 +0200

    Added the field definition for the series.

---
+++ src/MusicBrainz/Supplement/Browse/SeriesFields.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MusicBrainz\Supplement\Browse;
+
+class SeriesFields
+{
+    use \MusicBrainz\Supplement\Field\AliasesTrait;
+    use \MusicBrainz\Supplement\Field\AnnotationTrait;
+    use \MusicBrainz\Supplement\Field\RatingsTrait;
+    use \MusicBrainz\Supplement\Field\TagsTrait;
+    use \MusicBrainz\Supplement\Field\UserRatingsTrait;
+    use \MusicBrainz\Supplement\Field\UserTagsTrait;
+}
