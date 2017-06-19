60d5dc089074a4a2dafda32e6e66052e5c1d48b4
Christoph Hochholzer <christoph@hochholzer.info>
2017-06-19 20:28:33 +0200

    Added list object for works.

---
+++ src/MusicBrainz/Value/WorkList.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+/**
+ * A list of works
+ */
+class WorkList extends ValueList
+{
+    /**
+     * Constructs a list of works.
+     *
+     * @param array $works Information about the works
+     */
+    public function __construct(array $works = [])
+    {
+        parent::__construct(
+            array_map(
+                function($work) {
+                    return new Work($work);
+                },
+                $works
+            )
+        );
+    }
+
+    /**
+     * Returns the class name of the list elements.
+     *
+     * @return string
+     */
+    public static function getType(): string
+    {
+        return Work::class;
+    }
+}
