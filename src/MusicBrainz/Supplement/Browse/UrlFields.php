9087fe43c59ac3887ad9d1b6f37d7e00e9a57c4b
Christoph Hochholzer <christoph@hochholzer.info>
2017-06-29 23:56:38 +0200

    Added the field definition for the URL browse request.

---
+++ src/MusicBrainz/Supplement/Browse/UrlFields.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MusicBrainz\Supplement\Browse;
+
+class UrlFields
+{
+    use \MusicBrainz\Supplement\Field\AnnotationTrait;
+    use \MusicBrainz\Supplement\Field\TagsTrait;
+    use \MusicBrainz\Supplement\Field\UserTagsTrait;
+}
