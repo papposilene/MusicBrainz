7cdfec14664095aeb1e01a278a97c91690d5ca0c
Christoph Hochholzer <christoph@hochholzer.info>
2017-06-29 23:54:13 +0200

    Added a value object for a time stamp.

---
+++ src/MusicBrainz/Value/TimeStamp.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace MusicBrainz\Value;
+
+use MusicBrainz\Value;
+
+/**
+ * A day time
+ */
+class TimeStamp implements Value
+{
+    /**
+     * The hour
+     *
+     * @var null|int
+     */
+    private $hour;
+
+    /**
+     * The minute
+     *
+     * @var null|int
+     */
+    private $minute;
+
+    /**
+     * The time of day as string, formatted like 00:00, ..., 23:59
+     *
+     * @var string
+     */
+    private $time = '';
+
+    /**
+     * Constructs a time of day.
+     *
+     * @param string $time A time of day as string, formatted like 00:00, ..., 23:59
+     */
+    public function __construct(string $time = '')
+    {
+        if (preg_match('/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/', $time)) {
+            $this->hour   = (int) substr($time, 0, 2);
+            $this->minute = (int) substr($time, 3, 2);
+            $this->time   = $time;
+        }
+    }
+
+    /**
+     * Returns the hour.
+     *
+     * @return null|int
+     */
+    public function getHour(): ?int
+    {
+        return $this->hour;
+    }
+
+    /**
+     * Returns the minute.
+     *
+     * @return null|int
+     */
+    public function getMinute(): ?int
+    {
+        return $this->minute;
+    }
+
+    /**
+     * Returns the time of day as string, formatted like 00:00, ..., 23:59.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->time;
+    }
+}
