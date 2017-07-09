07d18482f8e412155b368b75888b4266bc96cac1
Christoph Hochholzer <christoph@hochholzer.info>
2017-07-09 16:13:52 +0200

    Enhanced the tests for searching annotations.

--- tests/Api/SearchTest.php
+++ tests/Api/SearchTest.php
@@ -4,7 +4,10 @@ namespace MusicBrainz\Test\Api;
 use MusicBrainz\Filter\Search\AnnotationFilter;
 use MusicBrainz\HttpAdapter\AbstractHttpAdapter;
 use MusicBrainz\MusicBrainz;
+use MusicBrainz\Value\Annotation;
 use MusicBrainz\Value\AnnotationText;
+use MusicBrainz\Value\EntityType;
+use MusicBrainz\Value\SearchResult;
 use MusicBrainz\Value\SearchResult\AnnotationList;
 use PHPUnit\Framework\TestCase;
 
@@ -41,8 +44,14 @@ class SearchTest extends TestCase
         $this->musicBrainz = new \MusicBrainz\MusicBrainz($this->httpAdapter);
     }
 
+    /**
+     * Tests, if Search::annotation() properly converts the given JSON response into a domain model and returns it.
+     *
+     * @return void
+     */
     public function testSearchAnnotaion(): void
     {
+        /** Setting up the mock object of the abstract HTTP adapter */
         $this->httpAdapter->expects($this->once())
             ->method('call')
             ->with(
@@ -52,22 +61,91 @@ class SearchTest extends TestCase
                     'limit'  => 5,
                     'offset' => 0,
                     'fmt'    => 'json',
-                    'query'  => 'text:lyrics'
+                    'query'  => 'text:awesome'
                 ]
             )
             ->willReturn(
                 json_decode(file_get_contents(__DIR__ . '/../Fixtures/Search/Annotation.json'), true)
             );
 
+        /** Performing the test $annotationFilter */
         $annotationFilter = new AnnotationFilter;
-        $annotationFilter->addAnnotationText(new AnnotationText('lyrics'));
+        $annotationFilter->addAnnotationText(new AnnotationText('awesome'));
         $pageFilter = new \MusicBrainz\Filter\PageFilter(0, 5);
 
         $result = $this->musicBrainz->api()->search()->annotation($annotationFilter, $pageFilter);
 
         $this->assertInstanceOf(
             AnnotationList::class,
-            $result
+            $result,
+            'Expected Search::annotation() to return a search result list of annotations.'
+        );
+
+        /** Validating the search result list */
+        $this->assertSame(
+            68,
+            $result->getCount()->getNumber(),
+            'Expected the total number of search results to be 68, as given in Fixtures/Search/Annotation.json.'
+        );
+
+        $this->assertSame(
+            0,
+            $result->getOffset()->getNumber(),
+            'Expected the offset to be 68, as given in Fixtures/Search/Annotation.json.'
+        );
+
+        $this->assertSame(
+            '2017/07/09 11:49:48',
+            (string) $result->getCreationTime(),
+            'Expected the creation time to be "2017/07/09 11:49:48", as given in Fixtures/Search/Annotation.json.'
+        );
+
+        /**
+         * Validating the first search result of the list
+         *
+         * @var SearchResult $searchResult The first search result of the list
+         */
+        $searchResult = $result[0];
+
+        $this->assertInstanceOf(
+            SearchResult::class,
+            $searchResult
+        );
+
+        $this->assertSame(
+            100,
+            $searchResult->getScore()->getNumber()
+        );
+
+        /**
+         * Validating the value of the first search result.
+         *
+         * @var Annotation $annotation The value of the first search result
+         */
+        $annotation = $searchResult->getValue();
+
+        $this->assertSame(
+            'Label: Awesome Arts - Cat#: AA001',
+            (string) $annotation->getAnnotationText(),
+            'Expected the text of the first annotation to be "Label: Awesome Arts - Cat#: AA001", as given in Fixtures/Search/Annotation.json.'
+        );
+
+        $this->assertSame(
+            '6370e01e-4fd4-49c4-858b-6f1df7c7fe91',
+            (string) $annotation->getMBID(),
+            'Expected the MusicBrainz Identifier (MBID) of the annotated entity to be "6370e01e-4fd4-49c4-858b-6f1df7c7fe91", as given in Fixtures/Search/Annotation.json.'
+        );
+
+        $this->assertSame(
+            EntityType::RELEASE,
+            (string) $annotation->getEntityType(),
+            'Expected the annotated entity to be of type release, as given in Fixtures/Search/Annotation.json.'
+        );
+
+        $this->assertSame(
+            'Echo Ricochet',
+            (string) $annotation->getName(),
+            'Expected the name of the annotated entity to be "Echo Ricochet", as given in Fixtures/Search/Annotation.json.'
         );
     }
 }
