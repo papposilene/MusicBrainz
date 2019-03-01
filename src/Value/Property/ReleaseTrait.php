fa64dfeabbb43e35605d93bef7ccfce7e1d34d73
XenosEleatikos <christoph.hochholzer.info>
2019-03-01 22:13:32 +0100

    Add optional key for setReleaseFromArray

--- src/Value/Property/ReleaseTrait.php
+++ src/Value/Property/ReleaseTrait.php
@@ -31,12 +31,20 @@ trait ReleaseTrait
      * Sets the release by extracting it from a given input array.
      *
      * @param array $input An array returned by the webservice
+     * @param null|string $key   Optional array key (default: "release")
+     *                           Use first array dimension for null
      *
      * @return void
      */
-    private function setReleaseFromArray(array $input): void
+    private function setReleaseFromArray(array $input, ?string $key = 'release'): void
     {
-        $this->release = is_null($release = ArrayAccess::getArray($input, 'release'))
+        if (is_null($key)) {
+            $this->release = new Release($input);
+
+            return;
+        }
+
+        $this->release = is_null($release = ArrayAccess::getArray($input, $key))
             ? new Release
             : new Release($release);
     }
